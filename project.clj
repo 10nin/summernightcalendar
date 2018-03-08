d94784cebf6aa687f3c4f8947b0f07df7495de38
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-08 22:59:28 +0900

    Add project dependencies.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject summernightcalendar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :description "Schedule arrangemnet web application for my hobby group."
+  :url "https://gitlab.com/10nin/summernightcalendar"
+  :license {:name "MIT Licnese"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.postgresql/postgresql "42.2.1.jre7"]])
