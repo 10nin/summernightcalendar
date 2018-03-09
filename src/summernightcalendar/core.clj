9ae30277f5f89ee82cae9df42cd4a3292ae7c8b5
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-09 23:04:59 +0900

    Add data-type record and functions stab.

--- src/summernightcalendar/core.clj
+++ src/summernightcalendar/core.clj
@@ -10,3 +10,32 @@
               :user (getenv "POSTGRES_USER")
               :password (getenv "POSTGRES_PASSWORD")
               })
+
+;;-- record definition
+(defrecord group- [group-name password])
+(defrecord group-activity- [group active-day active-type])
+(defrecord park-caendar- [opening-day opening-type])
+
+;;---- Insert functions ----
+(defn insert-group [group]
+  "Insert group information into GROUP_MASTER and LOGIN_INFORMATION_MASTER.")
+
+(defn insert-park-calendar [park-caendar]
+  "Insert park infromation into PARK_CALENDAR_MASTER.")
+  
+(defn insert-group-activity [group-activity]
+  "Insert group activity day into GROUP_CALENDAR")
+
+;;---- Select functions ----
+(defn select-month-calendar [month]
+  "Select :month calendar from GROUP_CALENDAR")
+
+(defn select-group-calendar [group]
+  "Select :group calendar from GROUP_CALENDAR")
+
+;;---- Web page handler ----
+(defn login-page-handler []
+  "Render login page and handling login event.")
+
+(defn monthly-calendar-handler [month]
+  "Render :month calendar page")
