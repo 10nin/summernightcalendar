c33eb5b8a4c185658a665522221d3e41634551eb
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-14 21:13:58 +0900

    Rewrite get-date function from single argument style to pattern matching.

--- src/summernightcalendar/calendar_util.clj
+++ src/summernightcalendar/calendar_util.clj
@@ -1,16 +1,17 @@
 (ns summernightcalendar.calendar-util)
 
-(defn get-date [f calendar field]
-  (f (.get calendar field)))
+(defn get-date
+  ([f calendar field] (f (.get calendar field)))
+  ([calendar field] (.get calendar field)))
 
 (defn get-year [calendar]
-  (get-date identity calendar java.util.Calendar/YEAR))
+  (get-date calendar java.util.Calendar/YEAR))
 
 (defn get-month [calendar]
   (get-date inc calendar java.util.Calendar/MONTH))
 
 (defn get-day [calendar]
-  (get-date identity calendar java.util.Calendar/DAY_OF_MONTH))
+  (get-date calendar java.util.Calendar/DAY_OF_MONTH))
 
 (defn get-first-day [year month]
   "Get java.util.Calendar object of year/month/1."
